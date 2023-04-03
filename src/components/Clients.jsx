b1efab0a4efc669139ce76c1d767d7fc698f3ca3
pseudoCoder <gonzalez.alejo2001@gmail.com>
2023-04-02 21:17:28 -0500

    Navlink clients added

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -2,7 +2,7 @@ import { clients } from '../constants';
 import styles from '../styles';
 
 const Clients = () => (
-  <section className={`${styles.marginY} flex flex-wrap items-center justify-center gap-6 sm:justify-between`}>
+  <section className={`${styles.marginY} flex flex-wrap items-center justify-center gap-6 sm:justify-between`} id="clients">
     {
       clients.map( (client, index)=>(
         <img className="w-[150px] sm:w-[200px]" src={client.logo} alt={client.id}/>
