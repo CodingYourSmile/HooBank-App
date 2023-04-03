4a0344be80aa8d4fc7fea6650bf1da3970f2a7a8
pseudoCoder <gonzalez.alejo2001@gmail.com>
2023-04-02 21:34:42 -0500

    CTA section finished and button modificated

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Button = () => (
-  <button className="self-center md:self-start w-[170px] h-[74px] rounded-md bg-blue-gradient text-[18px] font-medium font-poppins" type="button">Get Started</button>
+  <button className="w-[170px] h-[74px] rounded-md bg-blue-gradient text-[18px] font-medium font-poppins" type="button">Get Started</button>
 )
 
 export default Button
--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import styles from '../styles';
+import { Button } from '../components';
 
-const CTA = () => {
-  return (
-    <div>CTA</div>
-  )
-}
+const CTA = () => (
+  <section className={`${styles.marginY} flex flex-col md:flex-row md:justify-between items-center gap-6 p-6 sm:px-[90px] sm:py-[70px] w-full bg-black-gradient-2 rounded-xl`}>
+    <div className="max-w-[650px]">
+      <h1 className={`${styles.heading2}`}>Let's try our service now!</h1>
+      <p className={`${styles.paragraph}`}>Everything you need to accept card payments and grow your business anywhere on the planet.</p>
+    </div>
+    <Button/>
+  </section>
+)
+
 
 export default CTA
