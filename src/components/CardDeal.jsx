a8e834a67d6ab02b7bb08458a117f0469d28c27f
pseudoCoder <gonzalez.alejo2001@gmail.com>
2023-04-02 21:12:36 -0500

    Product navlink fixed

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -3,7 +3,7 @@ import { card } from '../assets';
 import { Button } from '../components';
 
 const CardDeal = () => (
-    <section className={`${styles.marginY} flex flex-col md:flex-row md:gap-20`}>
+    <section className={`${styles.marginY} flex flex-col md:flex-row md:gap-20`} id="product">
       <div className="flex flex-col gap-6">
         <h1 className={`${styles.heading2}`}>Find a better card deal in few easy steps</h1>
         <p className={`${styles.paragraph} text-justify`}>Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.</p>
