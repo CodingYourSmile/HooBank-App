41e03093b4421374a7b3032eee082a5443c04675
pseudoCoder <gonzalez.alejo2001@gmail.com>
2023-04-02 21:09:09 -0500

    Clients section finished

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { clients } from '../constants';
+import styles from '../styles';
 
-const Clients = () => {
-  return (
-    <div>Clients</div>
-  )
-}
+const Clients = () => (
+  <section className={`${styles.marginY} flex flex-wrap items-center justify-center gap-6 sm:justify-between`}>
+    {
+      clients.map( (client, index)=>(
+        <img className="w-[150px] sm:w-[200px]" src={client.logo} alt={client.id}/>
+      ) ) 
+    }
+  </section>
+)
+
 
 export default Clients
