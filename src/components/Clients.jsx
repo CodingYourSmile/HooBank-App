90dfc89dd074c9296c5dee630bce0352c6f8325d
pseudoCoder <gonzalez.alejo2001@gmail.com>
2023-04-04 00:47:11 -0500

    Footer section finished, new style added and fixed a problem with clients

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -5,7 +5,7 @@ const Clients = () => (
   <section className={`${styles.marginY} flex flex-wrap items-center justify-center gap-6 sm:justify-between`} id="clients">
     {
       clients.map( (client, index)=>(
-        <img className="w-[150px] sm:w-[200px]" src={client.logo} alt={client.id}/>
+        <img className="w-[150px] sm:w-[200px]" src={client.logo} alt={client.id} key={client.id}/>
       ) ) 
     }
   </section>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import { socialMedia, footerLinks } from '../constants';
+import { logo } from '../assets';
+import styles from '../styles';
 
-const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+const Footer = () => (
+  <footer className="flex flex-col gap-8 pb-8 md:pb-16">
+    <div className="flex flex-wrap justify-between gap-8">
+
+      <div className="flex flex-col items-center sm:items-start gap-8 sm:max-w-[350px]">
+        <img src={logo} alt="logo"/>
+        <p className={`${styles.paragraph}`}>A new way to make the payments easy, reliable and secure.</p>
+      </div>
+
+      {
+        footerLinks.map( (footer, index)=>(
+          <div key={`${footer.title}-${index}`}>
+            <p className={`${styles.footerText} font-bold`}>{footer.title}</p>
+            
+            <ul>
+              {
+                footer.links.map( (link, index)=>(
+                  <li className={`${styles.footerText}`} key={`${link.name}-${index}`}>
+                    <a href={link.link}>{link.name}</a>
+                  </li>
+                ) )
+              }
+            </ul>
+            
+          </div>
+        ) )
+      }
+
+    </div>
+
+    <div className="flex flex-col gap-4">
+      <div className="w-full h-px bg-black-gradient-2"/>
+      <div className="flex flex-wrap justify-center sm:justify-between items-center gap-4">
+        <p className={`${styles.footerText}`}>Copyright © 2021 HooBank. All Rights Reserved.</p>
+        <div className="flex items-center gap-4">
+          {
+            socialMedia.map( (social, index)=>(
+              <a key={social.id} href={social.link}> <img src={social.icon} alt={social.id}/> </a>
+            ) )
+          }
+        </div>
+        
+      </div>
+    </div>
+  </footer>
+)
+
 
 export default Footer
